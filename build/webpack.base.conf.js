392d8845cd821c318934930964cdf0b9ebe5c124
Stas <stas.chetyrbok@mail.ru>
2021-03-07 17:30:41 +0300

    UPDATE: fix the issue with fonts and imgs in built

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -39,55 +39,83 @@ module.exports = {
     }
   },
   module: {
-    rules: [{
-      test: /\.pug$/,
-      use: 'pug-loader'
-    }, {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.sass$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
-        }, {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
+    rules: [
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      }, 
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }, 
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/fonts/[name][ext]'
         }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+      }, 
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/img/[name][ext]'
         }
-      ]
-    }]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../'
+            }
+          },
+          {
+            loader: "css-loader",
+            options: { 
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+          },
+          {
+            loader: "stylus-loader",
+            options: { 
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, 
+          {
+            loader: 'postcss-loader',
+            options: { 
+              sourceMap: true, 
+              config: { path: `./postcss.config.js` }
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     alias: {
@@ -95,13 +123,11 @@ module.exports = {
     }
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin( ),
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].[hash].css`,
     }),
     new CopyWebpackPlugin([
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
       { from: `${PATHS.src}/static`, to: '' },
     ]),
     ...PAGES.map(page => new HtmlWebpackPlugin({
